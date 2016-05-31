7e358ace25c08558a40047c107a38d0774c1251b
PArns <github@patrick-arns.de>
2016-05-31 21:30:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+![Logo](admin/netatmo.png)
+# ioBroker.netatmo
+=================
+
+NetAtmo Adapter for ioBroker
+
+## Changelog
+
+#### 0.0.1
+* Initial release
+
+## License
+GPL V2
+
+Copyright (c) 2016 Patrick Arns <iobroker@patrick-arns.de>
---
+++ main.js
@@ -0,0 +1,48 @@
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+"use strict";
+
+var utils = require(__dirname + '/lib/utils');
+
+var adapter = utils.adapter('netatmo');
+
+adapter.on('unload', function (callback) {
+    try {
+        adapter.log.info('cleaned everything up...');
+        callback();
+    } catch (e) {
+        callback();
+    }
+});
+
+adapter.on('objectChange', function (id, obj) {
+    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+});
+
+adapter.on('stateChange', function (id, state) {
+    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
+
+    if (state && !state.ack) {
+        adapter.log.info('ack is not set!');
+    }
+});
+
+adapter.on('message', function (obj) {
+    if (typeof obj == 'object' && obj.message) {
+        if (obj.command == 'send') {
+            // e.g. send email or pushover or whatever
+            console.log('send command');
+
+            // Send response in callback if required
+            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+        }
+    }
+});
+
+adapter.on('ready', function () {
+    main();
+});
+
+function main() {
+    
+}

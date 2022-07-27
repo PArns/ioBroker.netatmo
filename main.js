fbd8db56159c845540d5e0518e25e7bd20cb9bdb
Ingo Fischer <github@fischer-ka.de>
2022-07-27 18:07:33 +0200

    * add token handling

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 
 const adapterName = require('./package.json').name.split('.').pop();
 const utils = require('@iobroker/adapter-core');
+const fs = require('fs');
 
 const netatmo = require('./lib/netatmoLib');
 let api = null;
@@ -34,6 +35,15 @@ let _smokedetectorUpdateInterval;
 let _cosensorUpdateInterval;
 let _doorbellUpdateInterval;
 
+let usedClientId;
+let usedClientSecret;
+let usedScopes;
+let storedOAuthData = {};
+let dataDir;
+
+const DEFAULT_CLIENT_ID = '574ddd152baa3cf9598b46cd';
+const DEFAULT_CLIENT_SECRET = '6e3UcBKp005k9N0tpwp69fGYECqOpuhtEE9sWJW';
+
 String.prototype.replaceAll = function (search, replacement) {
     const target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
@@ -62,6 +72,75 @@ function startAdapter(options) {
                     obj.callback && adapter.sendTo(obj.from, obj.command, {}, obj.callback);
 
                     break;
+                case 'getOAuthStartLink': {
+                    const args = obj.message;
+                    args.scope = getScopeList(args.scopes, !!(args.client_id && args.client_secret));
+                    if (args.client_id && args.client_secret) {
+                        args.client_id = args.client_id;
+                        args.client_secret = args.client_secret;
+                    } else {
+                        if (args.client_id || args.client_secret) {
+                            adapter.log.warn(`Only one of client_id or client_secret was set, using default values!`);
+                        }
+                        args.client_id = DEFAULT_CLIENT_ID;
+                        args.client_secret = DEFAULT_CLIENT_SECRET;
+                    }
+
+                    adapter.log.debug(`Get OAuth start link data: ${JSON.stringify(args)}`);
+                    const redirectData = api.getOAuth2AuthenticateStartLink(args);
+                    storedOAuthData[redirectData.state] = args;
+
+                    adapter.log.debug(`Get OAuth start link: ${redirectData.url}`);
+                    obj.callback && adapter.sendTo(obj.from, obj.command, redirectData.url, obj.callback);
+                    break;
+                }
+                case 'OAuthRedirectReceived': {
+                    const url = new URL(obj.message);
+                    if (!url.searchParams) {
+                        adapter.log.error('OAuthRedirectReceived: No search params');
+                        obj.callback && adapter.sendTo(obj.from, obj.command, 'OAuthRedirectReceived: No search params', obj.callback);
+                        break;
+                    }
+                    const args = {
+                        code: url.searchParams.get('code'),
+                        state: url.searchParams.get('state'),
+                    };
+                    adapter.log.debug('OAuthRedirectReceived: ' + JSON.stringify(args));
+
+                    api.authenticate(args, async err => {
+                        if (!err && storedOAuthData[args.state]) {
+                            const storedArgs = storedOAuthData[args.state];
+                            const native = storedArgs.scopes;
+                            if (api.client_id !== DEFAULT_CLIENT_ID) {
+                                native.id = api.client_id;
+                                native.secret = api.client_secret;
+                            }
+
+                            const tokenData = {
+                                access_token: api.access_token,
+                                refresh_token: api.refresh_token,
+                                scope: api.scope,
+                                client_id: api.client_id
+                            }
+                            try {
+                                adapter.log.info(`Save OAuth data: ${JSON.stringify(tokenData)}`);
+                                fs.writeFileSync(dataDir + '/tokens.json', JSON.stringify(tokenData), 'utf8');
+                            } catch (err) {
+                                adapter.log.error('Cannot write token file: ' + err);
+                            }
+
+                            adapter.log.info('Update data in adapter configuration ... restarting ...');
+                            adapter.extendForeignObject(`system.adapter.${adapter.namespace}`, {
+                                native
+                            });
+                        } else {
+                            adapter.log.error('OAuthRedirectReceived: ' + err);
+                        }
+                        obj.callback && adapter.sendTo(obj.from, obj.command, err, obj.callback);
+                    });
+
+                    break;
+                }
                 default:
                     adapter.log.warn('Unknown command: ' + obj.command);
                     break;
@@ -92,112 +171,170 @@ function startAdapter(options) {
         }
     });
 
-    adapter.on('ready', main);
+    adapter.on('ready', () => main());
+}
+
+function getScopeList(scopes, individualCredentials) {
+    let scope = '';
+
+    if (scopes.netatmoCoach) {
+        scope += ' read_homecoach';
+    }
+
+    if (scopes.netatmoWelcome) {
+        scope += ' read_camera read_presence';
+
+        if (individualCredentials) {
+            scope += ' access_camera access_presence write_camera'
+        } else {
+            adapter.log.info(`Welcome & Presence support limited because no individual ID/Secret provided.`);
+        }
+    }
+
+    if (scopes.netatmoSmokedetector) {
+        if (individualCredentials) {
+            scope += ' read_smokedetector';
+        } else {
+            adapter.log.warn(`Smoke detector only supported with individual ID/Secret. Disabling!`);
+            scopes.netatmoSmokedetector = false;
+        }
+    }
+
+    if (scopes.netatmoCOSensor) {
+        if (individualCredentials) {
+            scope += ' read_carbonmonoxidedetector';
+        } else {
+            adapter.log.warn(`CO sensor only supported with individual ID/Secret. Disabling!`);
+            scopes.netatmoCOSensor = false;
+        }
+    }
+
+    if (scopes.netatmoDoorBell) {
+        if (individualCredentials) {
+            scope += ' read_doorbell';
+        } else {
+            adapter.log.warn(`Doorbell only supported with individual ID/Secret. Disabling!`);
+            scopes.netatmoDoorBell = false;
+        }
+    }
+
+    // If nothing is set, activate at least the Weatherstation
+    if (!(scopes.netatmoCoach || scopes.netatmoWeather || scopes.netatmoWelcome || scopes.netatmoSmokedetector || scopes.netatmoCOSensor || scopes.netatmoDoorBell)) {
+        adapter.log.info('No product was chosen, using Weather station as default!');
+        scopes.netatmoWeather = true;
+    }
+
+    if (scopes.netatmoWeather) {
+        scope += ' read_station';
+    }
+
+    scope = scope.trim();
+
+    return scope;
 }
 
 function main() {
-    if (adapter.config.username && adapter.config.password) {
-        let scope = '';
-        let id = '574ddd152baa3cf9598b46cd';
-        let secret = '6e3UcBKp005k9N0tpwp69fGYECqOpuhtEE9sWJW';
-        let individualCredentials = false;
+    let scope = '';
+    let id = DEFAULT_CLIENT_ID;
+    let secret = DEFAULT_CLIENT_SECRET;
+    let individualCredentials = false;
+    let access_token;
+    let refresh_token;
 
-        if (adapter.config.id && adapter.config.secret) {
-            id = adapter.config.id;
-            secret = adapter.config.secret;
-            individualCredentials = true;
-            adapter.log.info(`Use individual ID/Secret`);
+    if (adapter.config.id && adapter.config.secret) {
+        id = adapter.config.id;
+        secret = adapter.config.secret;
+        individualCredentials = true;
+        adapter.log.info(`Use individual ID/Secret`);
+    }
+
+    scope = getScopeList(adapter.config, individualCredentials);
+
+    dataDir = utils.getAbsoluteInstanceDataDir(adapter);
+    try {
+        if (!fs.existsSync(dataDir)) {
+            fs.mkdirSync(dataDir);
         }
-
-        adapter.config.check_interval = parseInt(adapter.config.check_interval, 10);
-        adapter.config.cleanup_interval = parseInt(adapter.config.cleanup_interval, 10);
-
-        // we do not allow intervals below 5 minutes
-        if (!individualCredentials && (isNaN(adapter.config.check_interval) || adapter.config.check_interval < 10)) {
-            adapter.config.check_interval = 10;
-            adapter.log.warn(`Invalid check interval "${adapter.config.check_interval}", fallback to 10 minutes`);
-        }
-
-        if (!individualCredentials && (isNaN(adapter.config.cleanup_interval) || adapter.config.cleanup_interval < 20)) {
-            adapter.config.cleanup_interval = 60;
-            adapter.log.warn(`Invalid cleanup interval "${adapter.config.cleanup_interval}", fallback to 60 minutes`);
-        }
-
-        adapter.config.unknown_person_time = adapter.config.unknown_person_time || 24;
-
-        adapter.config.location_elevation = adapter.config.location_elevation || 0;
-
-        if (adapter.config.netatmoCoach) {
-            scope += ' read_homecoach';
-        }
-
-        if (adapter.config.netatmoWelcome) {
-            scope += ' read_camera read_presence';
-
-            if (individualCredentials) {
-                scope += ' access_camera access_presence write_camera'
+        if (fs.existsSync(dataDir + '/tokens.json')) {
+            const tokens = JSON.parse(fs.readFileSync(dataDir + '/tokens.json', 'utf8'));
+            if (tokens.client_id !== id) {
+                adapter.log.info(`Stored tokens belong to the different client ID ${tokens.client_id} and not to the configured ID ... deleting`);
+                fs.unlinkSync(dataDir + '/tokens.json');
             } else {
-                adapter.log.info(`Welcome & Presence support limited because no individual ID/Secret provided.`);
+                access_token = tokens.access_token;
+                refresh_token = tokens.refresh_token;
+                adapter.log.info(`Using stored tokens to initialize ...`);
+            }
+            if (tokens.scope !== scope) {
+                adapter.log.info(`Stored tokens have different scope ${tokens.scope} and not the configured scope ${scope} ... If you miss data please authenticate again!`);
             }
         }
+    } catch (err) {
+        adapter.log.error(`Error reading stored tokens: ${err.message}`);
+    }
 
-        if (adapter.config.netatmoSmokedetector) {
-            if (individualCredentials) {
-                scope += ' read_smokedetector';
-            } else {
-                adapter.log.warn(`Smoke detector only supported with individual ID/Secret. Disabling!`);
-                adapter.config.netatmoSmokedetector = false;
-            }
+    adapter.config.check_interval = parseInt(adapter.config.check_interval, 10);
+    adapter.config.cleanup_interval = parseInt(adapter.config.cleanup_interval, 10);
+
+    // we do not allow intervals below 5 minutes
+    if (!individualCredentials && (isNaN(adapter.config.check_interval) || adapter.config.check_interval < 10)) {
+        adapter.config.check_interval = 10;
+        adapter.log.warn(`Invalid check interval "${adapter.config.check_interval}", fallback to 10 minutes`);
+    }
+
+    if (!individualCredentials && (isNaN(adapter.config.cleanup_interval) || adapter.config.cleanup_interval < 20)) {
+        adapter.config.cleanup_interval = 60;
+        adapter.log.warn(`Invalid cleanup interval "${adapter.config.cleanup_interval}", fallback to 60 minutes`);
+    }
+
+    adapter.config.unknown_person_time = adapter.config.unknown_person_time || 24;
+
+    adapter.config.location_elevation = adapter.config.location_elevation || 0;
+
+    usedClientId = id;
+    usedClientSecret = secret;
+    usedScopes = scope;
+
+    const auth = {
+        'client_id': id,
+        'client_secret': secret,
+        'scope': scope,
+        'username': adapter.config.username,
+        'password': adapter.config.password
+    };
+    if (refresh_token) {
+        auth.access_token = access_token;
+        auth.refresh_token = refresh_token;
+    }
+
+    api = new netatmo();
+    api.setAdapter(adapter);
+
+    api.on('error', err => {
+        adapter.log.warn(`API Error: ${err.message}`);
+    });
+    api.on('warning', err => {
+        adapter.log.info(`API Warning: ${err.message}`);
+    });
+    api.on('access_token', access_token => {
+        adapter.log.debug(`Access Token: ${access_token}`);
+    });
+    api.on('refresh_token', refresh_token => {
+        adapter.log.debug(`Update Refresh tokens: ${refresh_token}`);
+        const tokenData = {
+            access_token: api.access_token,
+            refresh_token: api.refresh_token,
+            scope: api.scope,
+            client_id: api.client_id
         }
-
-        if (adapter.config.netatmoCOSensor) {
-            if (individualCredentials) {
-                scope += ' read_carbonmonoxidedetector';
-            } else {
-                adapter.log.warn(`CO sensor only supported with individual ID/Secret. Disabling!`);
-                adapter.config.netatmoCOSensor = false;
-            }
+        try {
+            fs.writeFileSync(dataDir + '/tokens.json', JSON.stringify(tokenData), 'utf8');
+        } catch (err) {
+            adapter.log.error('Cannot write token file: ' + err);
         }
-
-        if (adapter.config.netatmoDoorBell) {
-            if (individualCredentials) {
-                scope += ' read_doorbell';
-            } else {
-                adapter.log.warn(`Doorbell only supported with individual ID/Secret. Disabling!`);
-                adapter.config.netatmoDoorBell = false;
-            }
-        }
-
-        // If nothing is set, activate at least the Weatherstation
-        if (!(adapter.config.netatmoCoach || adapter.config.netatmoWeather || adapter.config.netatmoWelcome || adapter.config.netatmoSmokedetector || adapter.config.netatmoCOSensor || adapter.config.netatmoDoorBell)) {
-            adapter.log.info('No product was chosen, using Weather station as default!');
-            adapter.config.netatmoWeather = true;
-        }
-
-        if (adapter.config.netatmoWeather) {
-            scope += ' read_station';
-        }
-
-        scope = scope.trim();
-
-        const auth = {
-            'client_id': id,
-            'client_secret': secret,
-            'scope': scope,
-            'username': adapter.config.username,
-            'password': adapter.config.password
-        };
-
-        api = new netatmo(auth);
-
-        api.on('error', err => {
-            adapter.log.warn(`API Error: ${err.message}`);
-        });
-        api.on('warning', err => {
-            adapter.log.info(`API Warning: ${err.message}`);
-        });
-
-        api.setAdapter(adapter);
+    });
+    api.on('authenticated', () => {
+        adapter.log.debug(`Authenticated!!`);
 
         if (adapter.config.netatmoCoach) {
             coach = new NetatmoCoach(api, adapter);
@@ -252,8 +389,12 @@ function main() {
             _doorbellUpdateInterval = setInterval(() =>
                 doorbell.requestUpdateDoorBell(), adapter.config.check_interval * 2 * 60 * 1000);
         }
-    } else {
-        adapter.log.error('Please add username, password and choose at least one product within the adapter settings!');
+    });
+
+    try {
+        api.authenticate(auth);
+    } catch (err) {
+        adapter.log.error(`Error while authenticating: ${err.message}`);
     }
 }
 
